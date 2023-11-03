3620a2d1e0f664031e11545a9858c59dce53e321
koiie <koiie@yandex.ru>
2023-11-03 23:18:14 +0400

    Designed home page

---
+++ style/foot.php
@@ -0,0 +1,14 @@
+<div class="container">
+    <footer class="py-3 my-4">
+        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+            <li class="nav-item"><a href="/" class="nav-link px-2 text-body-secondary">Главная</a></li>
+            <li class="nav-item"><a href="/features" class="nav-link px-2 text-body-secondary">Функции</a></li>
+            <li class="nav-item"><a href="/faq" class="nav-link px-2 text-body-secondary">FAQs</a></li>
+            <li class="nav-item"><a href="/about" class="nav-link px-2 text-body-secondary">О нас</a></li>
+            <li class="nav-item"><a href="/contacts" class="nav-link px-2 text-body-secondary">Контакты</a></li>
+        </ul>
+        <p class="text-center text-body-secondary">&copy; 2023 VKPoster</p>
+    </footer>
+</div>
+</body>
+</html>
